6a3ee7178e40f95175164fa249c11319a9c0e6de
Allen Feng <59977850+yfeng445@users.noreply.github.com>
2024-09-16 14:44:19 -0400

    upd: INFO 5100 asmt1

---
+++ 1/INFO 5100/Asmts/Assignment 1/Person_Profile/src/profile/Person.java
@@ -0,0 +1,205 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package profile;
+
+/**
+ *
+ * @author 56111
+ */
+public class Person {
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getDlNum() {
+        return dlNum;
+    }
+
+    public void setDlNum(String dlNum) {
+        this.dlNum = dlNum;
+    }
+
+    public String getDlState() {
+        return dlState;
+    }
+
+    public void setDlState(String dlState) {
+        this.dlState = dlState;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getAddr1() {
+        return addr1;
+    }
+
+    public void setAddr1(String addr1) {
+        this.addr1 = addr1;
+    }
+
+    public String getAddr2() {
+        return addr2;
+    }
+
+    public void setAddr2(String addr2) {
+        this.addr2 = addr2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getFax() {
+        return fax;
+    }
+
+    public void setFax(String fax) {
+        this.fax = fax;
+    }
+
+    public String getFavAnimal() {
+        return favAnimal;
+    }
+
+    public void setFavAnimal(String favAnimal) {
+        this.favAnimal = favAnimal;
+    }
+
+    public String getFavFood() {
+        return favFood;
+    }
+
+    public void setFavFood(String favFood) {
+        this.favFood = favFood;
+    }
+
+    public String getFavSport() {
+        return favSport;
+    }
+
+    public void setFavSport(String favSport) {
+        this.favSport = favSport;
+    }
+
+    public String getFavMusic() {
+        return favMusic;
+    }
+
+    public void setFavMusic(String favMusic) {
+        this.favMusic = favMusic;
+    }
+
+    public String getFavBook() {
+        return favBook;
+    }
+
+    public void setFavBook(String favBook) {
+        this.favBook = favBook;
+    }
+
+    public String getLangPref() {
+        return langPref;
+    }
+
+    public void setLangPref(String langPref) {
+        this.langPref = langPref;
+    }
+
+    // Must-included attributes
+    String fName;
+    String lName;
+    String email;
+    String phone;
+    String dlNum;
+    String dlState;
+    String ssn;
+    String addr1;
+    String addr2;
+    String city;
+    String state;
+    String zip;
+    String tel;
+    String fax;
+    
+    // Additional attributes
+    String favAnimal;
+    String favFood;
+    String favSport;
+    String favMusic;
+    String favBook;
+    String langPref;
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
